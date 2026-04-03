b7b249b8a25a52a2384fa63ab15fa61afa8e86bd
Ofri Gal <ofri0101@gmail.com>
2026-04-03 18:54:24 +0300

    updated the csv parser and the item list

--- csv_parser.py
+++ csv_parser.py
@@ -1,8 +1,15 @@
+import os
 import pandas as pd
 
 df=pd.read_csv('basic_items.csv')
-print(df.head())
+df = df[df["Group"] != "Vial"]
+df = df[df["Group"] != "Artifact"]
+df = df[df["Group"] != "Capsule"]
+df = df[~df["Config"].str.contains("Guild")]
+df = df[~df["Config"].str.contains("Ticket/Variants/Custom")]
+# print(df.head())
 ITEM_LIST = set(df['Name'].tolist())
+os.remove('items.txt') if os.path.exists('items.txt') else None
 with open('items.txt', 'x', encoding='utf-8') as f:
     f.write("[")
     for item in sorted(ITEM_LIST):
