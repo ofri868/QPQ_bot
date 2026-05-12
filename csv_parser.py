0e96f5c3e44e40f3379fb66ebbef3f01bb84f21c
Ofri Gal <ofri0101@gmail.com>
2026-05-12 07:34:33 +0300

    made function to create item list

--- csv_parser.py
+++ csv_parser.py
@@ -1,4 +1,5 @@
 import os
+import re
 import pandas as pd
 
 df=pd.read_csv('basic_items.csv')
@@ -10,13 +11,64 @@ df = df[~df["Config"].str.contains("/Guild/")]
 df = df[~df["Config"].str.contains("Harness")]
 df = df[~df["Config"].str.contains("Ticket/Variants/Custom")]
 df = df[~df["Config"].str.contains("Furni/Trophy/Boss Trophy")]
+df = df[~df["Config"].str.contains("Game Master")]
 # print(df.head())
-ITEM_LIST = set(df['Name'].tolist())
 os.remove('items.txt') if os.path.exists('items.txt') else None
 with open('items.txt', 'x', encoding='utf-8') as f:
     f.write("[")
-    for item in sorted(ITEM_LIST):
-        f.write(f"\"{item}\",")
+    for index, row in df.iterrows():
+        if re.match(r'^Weapon|^Gear', row['Config']):
+            f.write("{"+f"\"{row['Name']}\", \"Gear\""+"},")
+        elif re.match(r'^Costume', row['Config']):
+            f.write("{"+f"\"{row['Name']}\", \"Costumes\""+"},")
+        elif re.match(r'^Accessory/Armor/Aura', row['Config']):
+            f.write("{"+f"\"{row['Name']}\", \"Armor Aura\""+"},")
+        elif re.match(r'^Accessory/Armor/Back', row['Config']):
+            if re.match(r'.tail.|.extension cord.', row['Config'], re.IGNORECASE):
+                f.write("{"+f"\"{row['Name']}\", \"Armor Rear\""+"},")
+            elif re.match(r'.ankle.|.slippers.|.trotters.', row['Config'], re.IGNORECASE):
+                f.write("{"+f"\"{row['Name']}\", \"Armor Ankle\""+"},")
+            f.write("{"+f"\"{row['Name']}\", \"Armor Back\""+"},")
+        elif re.match(r'^Accessory/Armor/Front', row['Config']):
+            f.write("{"+f"\"{row['Name']}\", \"Armor Front Arms\""+"},")
+        elif re.match(r'^Accessory/Helm/Back', row['Config']):
+            f.write("{"+f"\"{row['Name']}\", \"Helm Back\""+"},")
+        elif re.match(r'^Accessory/Helm/Front', row['Config']):
+            f.write("{"+f"\"{row['Name']}\", \"Helm Front\""+"},")
+        elif re.match(r'^Accessory/Helm/Side', row['Config']):
+            f.write("{"+f"\"{row['Name']}\", \"Helm Side\""+"},")
+        elif re.match(r'^Accessory/Helm/Top', row['Config']):
+            f.write("{"+f"\"{row['Name']}\", \"Helm Top Brow\""+"},")
+        else:
+            f.write("{"+f"\"{row['Name']}\", \"Misc\""+"},")
     f.seek(f.tell() - 1)  # Move the file pointer back to overwrite the last comma
     f.write("]")
-print(len(ITEM_LIST))
+def parse_csv():
+    with open('items.txt', 'r', encoding='utf-8') as f:
+        items = []
+        for index, row in df.iterrows():
+            if re.match(r'^Weapon|^Gear', row['Config']):
+                items.append({'name': row['Name'], 'type': 'Gear'})
+            elif re.match(r'^Costume', row['Config']):
+                items.append({'name': row['Name'], 'type': 'Costumes'})
+            elif re.match(r'^Accessory/Armor/Aura', row['Config']):
+                items.append({'name': row['Name'], 'type': 'Armor Aura'})
+            elif re.match(r'^Accessory/Armor/Back', row['Config']):
+                if re.match(r'.tail.|.extension cord.', row['Config'], re.IGNORECASE):
+                    items.append({'name': row['Name'], 'type': 'Armor Rear'})
+                elif re.match(r'.ankle.|.slippers.|.trotters.', row['Config'], re.IGNORECASE):
+                    items.append({'name': row['Name'], 'type': 'Armor Ankle'})
+                items.append({'name': row['Name'], 'type': 'Armor Back'})
+            elif re.match(r'^Accessory/Armor/Front', row['Config']):
+                items.append({'name': row['Name'], 'type': 'Armor Front Arms'})
+            elif re.match(r'^Accessory/Helm/Back', row['Config']):
+                items.append({'name': row['Name'], 'type': 'Helm Back'})
+            elif re.match(r'^Accessory/Helm/Front', row['Config']):
+                items.append({'name': row['Name'], 'type': 'Helm Front'})
+            elif re.match(r'^Accessory/Helm/Side', row['Config']):
+                items.append({'name': row['Name'], 'type': 'Helm Side'})
+            elif re.match(r'^Accessory/Helm/Top', row['Config']):
+                items.append({'name': row['Name'], 'type': 'Helm Top Brow'})
+            else:
+                items.append({'name': row['Name'], 'type': 'Misc'})
+        return items
